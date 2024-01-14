d4e68546e725f7639e14aad052d81ead3f73fd48
Miguel Cuadros <cuadros_miguel@hotmail.com>
2024-01-14 03:59:18 -0300

    sync x3 updated

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import app from "./app.js";
-import { db } from "./config/config.js";
 import { PORT } from "./config/config.js";
-import { sequelize } from "./config/cloud_database.js";
+import { sequelize as cloudDB } from "./config/cloud_database.js";
+import { sequelize as localDB } from "./config/local_database.js";
+import { sequelize as remoteDB } from "./config/database.js";
 // import "./models/Usuario.js";
 // import "./models/Rol.js";
 
-import "./libs/configuracionInicial.js"
+import "./libs/configuracionInicial.js";
+
+const sincronizarModelos = async (dbConfig) => {
+  try {
+    await dbConfig.sync();
+    console.log(`Modelos sincronizados para ${dbConfig.config.database} correctamente.`);
+  } catch (error) {
+    console.error(`Error al sincronizar modelos para ${dbConfig.config.database}:`, error);
+    throw error;
+  }
+};
 
 async function main() {
   try {
+    // Sincronizar modelos para cada base de datos
+    await sincronizarModelos(cloudDB);
+    await sincronizarModelos(localDB);
+    await sincronizarModelos(remoteDB);
+
     console.log("Connection has been established successfully.");
 
     // Sincroniza las tablas después de que el servidor esté escuchando
